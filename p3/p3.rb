b7d358e29c91fa29e033cf9b1d67de035fd99fab
Ryan Liang <ryanliang@gmail.com>
2013-05-21 12:59:12 -0400

    use a diff algorithm to improve performance

--- p3/p3.rb
+++ p3/p3.rb
@@ -1,19 +1,23 @@
 # http://projecteuler.net/problem=3
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
-SMALLEST_PRIM = 3
+SMALLEST_PRIM = 2
 
 def p3
   num = 600851475143
-  # num = 13775
-  result = 0
-  num.downto(SMALLEST_PRIM) do |n|
-    if num % n == 0 && prime?(n) then
-      result = n
-      break
-    end
+  keep_going = true
+  result, factor = 0
+  while (keep_going) do
+    factor = find_prime_factor(num)
+    result = factor if factor > result
+    num = num / factor
+    keep_going = false if prime?(num)
   end
-  result
+  return result > num ? result : num
+end
+
+def find_prime_factor(num)
+  (SMALLEST_PRIM..num).each { |e| return e if num % e == 0 && prime?(e)}
 end
 
 def prime?(num)
@@ -24,5 +28,4 @@ def prime?(num)
   return true
 end
 
-# puts prime?(600851475143)
 puts p3

e955c60cb4e7adfe35cd087bb2868d5a3ddea26a
Ryan Liang <ryanliang@gmail.com>
2013-05-21 09:48:08 -0400

    slight touch on syntax

--- p1/p1.rb
+++ p1/p1.rb
@@ -1,11 +1,12 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
+# Q: If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
 # get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the 
 # multiples of 3 or 5 below 1000.
 
-def p1
-  count = 999
+# A: 233168
+
+def p1  
   sum = 0 
-  for n in 1..count do
+  (1..999).each do |n|
     if n % 3 == 0 || n % 5 == 0 then
       sum = sum + n
     end

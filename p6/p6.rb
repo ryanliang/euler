135005678d036e46ecbecde897dd7a813cb2addd
Ryan Liang <ryanliang@gmail.com>
2013-05-21 14:35:08 -0400

    added p6 link

--- p6/p6.rb
+++ p6/p6.rb
@@ -1,12 +1,11 @@
+# http://projecteuler.net/problem=6
 def p6
   sum_square = 0
   (1..100).each { |e| sum_square += e }
-  sum_square = sum_square * sum_square
-  puts sum_square
+  sum_square = sum_square * sum_square  
 
   square_sum = 0
-  (1..100).each { |e| square_sum += e * e }
-  puts square_sum
+  (1..100).each { |e| square_sum += e * e }  
 
   sum_square - square_sum
 end
